b3a4da28d62f9ee260153f3714a5e4b5c7ad5939
hgaur1089 <hgaur1089@gmail.com>
2022-03-27 15:06:56 +0530

    subscribe to store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
 
 import App from "./components/App";
 import "./index.css";
+import movies from './reducers';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(movies);
+// console.log(store.getState());
+
+// store.dispatch({
+//   type: 'ADD_MOVIES',
+//   movies: [{ name: 'Superman' }]
+// });
+
+// console.log(store.getState());
+
+ReactDOM.render(<App store={store}/>, document.getElementById("root"));
