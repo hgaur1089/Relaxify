8da2056c8727e8aa61468dfd3585a3dde4826768
hgaur1089 <hgaur1089@gmail.com>
2022-03-30 08:06:43 +0530

    2 files modified

--- src/components/App.js
+++ src/components/App.js
@@ -1,100 +1,88 @@
 import React from 'react';
-import { data } from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 import { addMovies, setShowFavourites } from '../actions';
-import {StoreContext} from '../index';
+import { data as moviesList } from '../data';
+import { connect } from '../index';
 
 class App extends React.Component {
   componentDidMount() {
-    const { store } = this.props;
-    store.subscribe(() => {
-      console.log('UPDATED');
-      this.forceUpdate();
-    });
-    //make api call
-    //dispatch action
-    store.dispatch(addMovies(data));
-
-    console.log('ss', this.props.store.getState());
+    this.props.dispatch(addMovies(moviesList));
   }
 
-  isMovieFavourite = (movie) => {
-    const {movies} = this.props.store.getState();
-    const { favourites } = movies;
-    const index = favourites.indexOf(movie);
+  isMovieInFavourites = (movie) => {
+    const { movies } = this.props;
 
+    const index = movies.favourites.indexOf(movie);
     if (index !== -1) {
-      //movie found
       return true;
     }
+
     return false;
   };
 
-  onChangeTab = (value) => {
-    const { showFavourites } = this.props.store.getState(); 
-    // { movies: {}, search: {}}
-    this.props.store.dispatch(setShowFavourites(value));
+  changeTab = (val) => {
+    this.props.dispatch(setShowFavourites(val));
   };
-
   render() {
-    console.log('render', this.props.store.getState());
-    const { movies, search } = this.props.store.getState();
-    const { list, favourites, showFavourites } = movies;
-
+    const { movies, search } = this.props; // will return { movies: {}, search: []}
+    console.log('movies', movies);
+    const { list, showFavourites = [], favourites = [] } = movies;
     const displayMovies = showFavourites ? favourites : list;
 
     return (
       <div className="App">
-        <Navbar search={search}/>
+        <Navbar search={search} />
         <div className="main">
           <div className="tabs">
             <div
               className={`tab ${showFavourites ? '' : 'active-tabs'}`}
-              onClick={() => {
-                this.onChangeTab(false);
-              }}
+              onClick={() => this.changeTab(false)}
             >
               Movies
             </div>
             <div
               className={`tab ${showFavourites ? 'active-tabs' : ''}`}
-              onClick={() => {
-                this.onChangeTab(true);
-              }}
+              onClick={() => this.changeTab(true)}
             >
               Favourites
             </div>
           </div>
 
-          <div className="list">
-            {displayMovies.map((movie, index) => (
+          <div id="list">
+            {displayMovies.map((movie) => (
               <MovieCard
                 movie={movie}
-                key={`movies-${index}`}
-                dispatch={this.props.store.dispatch}
-                isFavourite={this.isMovieFavourite(movie)}
+                key={movie.imdbID}
+                dispatch={this.props.dispatch}
+                isFavourite={this.isMovieInFavourites(movie)}
               />
             ))}
+            {displayMovies.length === 0 ? (
+              <div className="no-movies">No movies to display! </div>
+            ) : null}
           </div>
-
-          {displayMovies.length === 0 ? (
-            <div className="no-movies">No Movies to Show</div>
-          ) : null}
         </div>
       </div>
     );
   }
 }
 
-class AppWrapper extends React.Component {
-  render(){
-    return (
-      <StoreContext.Consumer>
-        {(store) => <App store={store} />}
-      </StoreContext.Consumer>
-    );
-  }
-}
+// class AppWrapper extends React.Component {
+//   render() {
+//     return (
+//       <StoreContext.Consumer>
+//         {(store) => <App store={store} />}
+//       </StoreContext.Consumer>
+//     );
+//   }
+// }
 
-export default AppWrapper;
+function mapStateToProps(state) {
+  return {
+    movies: state.movies,
+    search: state.movies,
+  };
+}
+const connectedComponent = connect(mapStateToProps)(App);
+export default connectedComponent;
--- src/index.js
+++ src/index.js
@@ -4,55 +4,96 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 import App from './components/App';
-import './index.css';
 import rootReducer from './reducers';
+import './index.css';
 
-// const logger = function({ dispatch, getState}) {
-//   return function (next) {
-//     return function (action){
-//       console.log('action type ', action.type);
+// const logger = function({ dispatch, getState }) {
+//   return function(next) {
+//     return function(action) {
+//       // my middlware
+//       console.log('ACTION', action);
 //       next(action);
-//     }
-//   }
-// }
+//     };
+//   };
+// };
 
-const logger =
-  ({ dispatch, getState }) =>
-  (next) =>
-  (action) => {
-    if (typeof action !== 'function') console.log('action type ', action.type);
-    next(action);
-  };
+const logger = ({ dispatch, getState }) => (next) => (action) => {
+  // my middlware
+  console.log('ACTION', action);
+  next(action);
+};
 
-// const thunk = ({dispatch, getState}) => (next) => (action) => {
-//   if(typeof action === 'function'){
-//     action(dispatch);
-//     return;
+// const thunk = store => next => action => {
+//   if (typeof action === 'function') {
+//     return action(store.dispatch);
 //   }
+
 //   next(action);
-// }
+// };
 
 const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+// console.log(store);
 console.log('state', store.getState());
 
 export const StoreContext = createContext();
+
 console.log('StoreContext', StoreContext);
 
 class Provider extends React.Component {
   render() {
     const { store } = this.props;
-    return <StoreContext.Provider value={store}>
-      {this.props.children}
-    </StoreContext.Provider>;
+    return (
+      <StoreContext.Provider value={store}>
+        {this.props.children}
+      </StoreContext.Provider>
+    );
   }
 }
 
+// const connectedComponent = connect(callback)(App);
+export function connect(callback) {
+  return function (Component) {
+    class ConnectedComponent extends React.Component {
+      constructor(props) {
+        super(props);
+        this.unsubscribe = this.props.store.subscribe(() => {
+          this.forceUpdate();
+        });
+      }
+
+      componentWillUnmount() {
+        this.unsubscribe();
+      }
+      render() {
+        const { store } = this.props;
+        const state = store.getState();
+        const dataToBeSentAsProps = callback(state);
+
+        return <Component dispatch={store.dispatch} {...dataToBeSentAsProps} />;
+      }
+    }
+
+    class ConnectedComponentWrapper extends React.Component {
+      render() {
+        return (
+          <StoreContext.Consumer>
+            {(store) => {
+              return <ConnectedComponent store={store} />;
+            }}
+          </StoreContext.Consumer>
+        );
+      }
+    }
+    return ConnectedComponentWrapper;
+  };
+}
+
+// update store by dispatching actions
 // store.dispatch({
 //   type: 'ADD_MOVIES',
-//   movies: [{ name: 'Superman' }]
+//   movies: moviesList
 // });
-
-// console.log(store.getState());
+// console.log('state', store.getState());
 
 ReactDOM.render(
   <Provider store={store}>
